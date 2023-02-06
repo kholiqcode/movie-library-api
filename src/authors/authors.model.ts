d2e858aa7ae54b5e72c3844247cc18edb91cb76b
Kholiq <kholiqcode@gmail.com>
2023-02-06 08:30:27 +0700

    feat: create authors model

---
+++ src/authors/authors.model.ts
@@ -0,0 +1,33 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import {
+  AutoIncrement,
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+} from 'sequelize-typescript';
+
+@ObjectType()
+export class Author extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  @Field((type) => Int)
+  id: number;
+
+  @Column
+  @Field(() => String)
+  title: string;
+
+  @Column(DataType.DATEONLY)
+  @Field(() => String)
+  birthDate: string;
+
+  @Column(DataType.DATE)
+  @Field(() => String)
+  createdAt: string;
+
+  @Column(DataType.DATE)
+  @Field(() => String)
+  updatedAt: string;
+}
