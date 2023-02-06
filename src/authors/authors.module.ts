4238f543041de068c18641c04a66ee7cf9846f03
Kholiq <kholiqcode@gmail.com>
2023-02-06 08:39:14 +0700

    feat: import model to module

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { AuthorsResolver } from './authors.resolver';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Author } from './authors.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Author])],
   providers: [AuthorsResolver, AuthorsService],
 })
 export class AuthorsModule {}
--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesResolver } from './movies.resolver';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Movie } from './movies.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Movie])],
   providers: [MoviesResolver, MoviesService],
 })
 export class MoviesModule {}
