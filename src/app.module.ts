cd6df7d193faa86027ff452dbf1bb2543d0ede86
Kholiq <kholiqcode@gmail.com>
2023-02-06 08:03:09 +0700

    feat: create movie resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AppService } from './app.service';
       autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
       sortSchema: true,
     }),
+    MoviesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
