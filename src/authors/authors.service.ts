4f51684b93300c9a1d7c6b0e43af2cca5552a196
Kholiq <kholiqcode@gmail.com>
2023-02-06 10:25:17 +0700

    feat: get by id author

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -6,7 +6,7 @@ import { UpdateAuthorInput } from './dto/update-author.input';
 
 @Injectable()
 export class AuthorsService {
-  constructor(@InjectModel(Author) private auhtorRepo: typeof Author) {}
+  constructor(@InjectModel(Author) private authorRepo: typeof Author) {}
 
   async create(createAuthorInput: CreateAuthorInput): Promise<string> {
     const author: Author = new Author();
@@ -17,11 +17,15 @@ export class AuthorsService {
   }
 
   findAll(): Promise<Author[]> {
-    return this.auhtorRepo.findAll();
+    return this.authorRepo.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} author`;
+  findOne(id: number): Promise<Author> {
+    return this.authorRepo.findOne({
+      where: {
+        id,
+      },
+    });
   }
 
   update(id: number, updateAuthorInput: UpdateAuthorInput) {
