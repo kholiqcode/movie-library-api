e06eabb0655ce44754a04466ffbc7fd6b5362640
Kholiq <kholiqcode@gmail.com>
2023-02-06 08:04:59 +0700

    feat: create authors resouce

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { ActorsModule } from './actors/actors.module';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { ActorsModule } from './actors/actors.module';
     }),
     MoviesModule,
     ActorsModule,
+    AuthorsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
